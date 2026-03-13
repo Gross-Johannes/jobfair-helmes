a682077847b7ced4214611d8d8385d276ca78530
Johannes Gross <johannesgross02@gmail.com>
2026-03-13 13:22:46 +0200

    chore(backend): initial backend set up

---
+++ backend/src/main/java/com/example/helmes_challenge/HelmesChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.helmes_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelmesChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelmesChallengeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/helmes_challenge/HelmesChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helmes_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelmesChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
